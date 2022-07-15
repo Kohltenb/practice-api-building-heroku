62078a7efc36fd15aa66eb00995e66cbfa0f5829
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-15 15:52:26 -0600

    First commit getting started

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+
+const server = express()
+const PORT = process.env.PORT || 8080;
+
+server.use(express.json())
+
+server.get('/api/user', (req, res) => {
+    res.json([
+        { id: 1, username: 'foo'},
+        { id: 2, username: 'bar'},
+        { id: 3, username: 'baz'}
+    ])
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Listening on, ${PORT}!`)
+})
